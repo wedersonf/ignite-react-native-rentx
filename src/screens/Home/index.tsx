3cc1053e0605846f3605544144b2e0f9720718b0
wedersonf <wedymf@hotmail.com>
2021-09-19 20:58:58 -0300

    Create home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
+
+import {
+  Container,
+  Header,
+  TotalCars,
+  CarList
+} from './styles';
+
+export function Home(){
+  const carData = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+    
+  }
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo
+          width={RFValue(108)}
+          height={RFValue(12)}
+        />
+
+        <TotalCars>
+          Total de 12 carros
+        </TotalCars>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
+    </Container>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { FlatList } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  padding: 32px 24px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const CarList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 24
+  },
+  showsVerticalScrollIndicator: false
+})``;
