8c6586958b8c15a64218e3a9f07f4fe06d0e2189
wedersonf <wedymf@hotmail.com>
2021-09-27 22:33:58 -0300

    Schedule complete

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 
-import { ScheduleDetails } from './src/screens/ScheduleDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 import theme from './src/styles/theme';
 
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <ScheduleDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   )
 }
