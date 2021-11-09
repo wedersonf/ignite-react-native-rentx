ba38a42c8d11ebcb1564cbc2517f6d8422436191
wedersonf <wedymf@hotmail.com>
2021-11-09 17:46:00 -0300

    fix sync

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,6 +30,7 @@ interface AuthContextData {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
   updateUser: (user: User) => Promise<void>;
+  loading: boolean;
 }
 
 interface AuthProviderProps {
@@ -40,6 +41,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [data, setData] = useState<User>({} as User);
+  const [loading, setLoading] = useState(true);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -111,6 +113,7 @@ function AuthProvider({ children }: AuthProviderProps) {
         const userData = response[0]._raw as unknown as User;
         api.defaults.headers.authorization = `Bearer ${userData.token}`;
         setData(userData);
+        setLoading(false);
       }
     }
 
@@ -123,7 +126,8 @@ function AuthProvider({ children }: AuthProviderProps) {
         user: data,
         signIn,
         signOut,
-        updateUser
+        updateUser,
+        loading
       }}
     >
       {children}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { useAuth } from '../hooks/auth';
 
-import { AppStackRoutes } from './app.stack.routes';
 import { AppTabRoutes } from './app.tab.routes';
 import { AuthRoutes } from './auth.routes';
+import { LoadAnimation } from '../components/LoadAnimation';
 
 export function Routes(){
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
   return (
+    loading ? <LoadAnimation /> :
     <NavigationContainer>
       { user.id ? <AppTabRoutes /> : <AuthRoutes /> }
     </NavigationContainer>
